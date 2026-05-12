4e6b366a8c0139cc7e8bb84c952f2a114feafc07
A idank <129357175+Aidank-cy@users.noreply.github.com>
2026-05-12 23:43:07 +0800

    Set basePath and assetPrefix for portfolio

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Aidank-portfolio",
+  assetPrefix: "/Aidank-portfolio/",
   images: {
     unoptimized: true,
   },
