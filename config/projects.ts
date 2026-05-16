270216283cb190e282724696559e84521b60bdbd
unknown <hezhengdong95@gmail.com>
2026-05-16 23:26:19 +0800

    feat/uppercasesigma

--- config/projects.ts
+++ config/projects.ts
@@ -1,12 +1,12 @@
 export const projects = [
   {
+    repo: "Aidank-cy/SIGMA",
+    featured: true,
+    order: 1,
+  },
+  {
     repo: "Aidank-cy/Aidank-portfolio",
     featured: true,
     order: 2,
-  },
-  {
-    repo: "Aidank-cy/sigma",
-    featured: true,
-    order: 1,
   },
 ] as const;
