5f491e9efd5e8b7c3537b10792693ed8472624b0
unknown <hezhengdong95@gmail.com>
2026-05-16 20:11:53 +0800

    feat:add-sigma

--- config/projects.ts
+++ config/projects.ts
@@ -2,6 +2,11 @@ export const projects = [
   {
     repo: "Aidank-cy/Aidank-portfolio",
     featured: true,
+    order: 2,
+  },
+  {
+    repo: "Aidank-cy/sigma",
+    featured: true,
     order: 1,
   },
 ] as const;
